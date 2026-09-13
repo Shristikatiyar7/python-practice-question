eaf6acba98f6fc670be96ddc8231fd6a025e4dc2
Shristikatiyar7 <katiyarshristi9@gmail.com>
2026-09-13 22:22:54 +0530

    Add percentage calculation based on obtained marks

---
+++ question 5.py
@@ -0,0 +1,3 @@
+obtained_marks = float(input("Enter the obtained marks: ").strip())
+Total_marks = float(input("Enter the total marks: ").strip())
+print(f'percentage: {(obtained_marks / Total_marks) * 100:.2f}')
