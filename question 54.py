8e01cb1d3120a7e636ef8c9d201a894bb64373d0
Shristikatiyar7 <katiyarshristi9@gmail.com>
2026-09-13 23:21:19 +0530

    Create question 54.py

---
+++ question 54.py
@@ -0,0 +1,9 @@
+n = int(input())
+d = int(input())
+
+for i in range(2, n):
+    if n % i == 0 and i == d:
+        print("Targetdivisorfound")
+        break 
+else:
+    print("Targetdivisornotfound")
